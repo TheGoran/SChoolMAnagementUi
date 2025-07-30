8c5cea8ba8605ffaa093a5255e44e0db0006cbc4
TheGoran <42156001+TheGoran@users.noreply.github.com>
2025-07-31 00:20:37 +0100

    Event CalendarcFix
    Deleted the whole file by accident lol
    Made Loogo on dashboard layout bold.
    Corrected the img to Image from next/Image
    this is @1h20min8sec...moving on

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import Menu from "@/components/Menu";
 import Navbar from "@/components/Navbar"
+import Image from "next/image";
 
 export default function RootLayout({
     children,
@@ -12,8 +13,8 @@ export default function RootLayout({
       {/* {We gonna style the left side smaller than the right side} */}
       <div className="w-[14%] md-:w-[8%] lg:w-[16%] xl:w-[14%]">
           <Link href="/" className="flex items-center justify-center lg:justify-start p-4 gap-2">
-              <img src="/logo.png" alt="logo" width={32} height={32}/>
-              <span className="hidden lg:block">Go School Management</span>
+              <Image src="/logo.png" alt="logo" width={32} height={32} />
+              <span className="hidden lg:block font-bold">Go School Management</span>
           </Link>
           <Menu>
 
